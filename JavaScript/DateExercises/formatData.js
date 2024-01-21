71940a69defabc91783a29c9d94804f12c8235e3
Maria Luisa Freitas de Lucena <malufreitasdelucena@gmail.com>
2024-01-21 22:58:44 +0000

    Changes data and loop exercises to a better written code

--- JavaScript/DateExercises/formatData.js
+++ JavaScript/DateExercises/formatData.js
@@ -4,15 +4,15 @@ function formataData(data){
     const MONTH = data.getMonth() + 1;
     const YEAR = data.getFullYear();
     if (DAY < 10 && MONTH < 10){
-        return `0${DAY}-0${MONTH}-${YEAR}`;
+        return `0${MONTH}-0${DAY}-${YEAR}`;
     }
-    else if (DAY < 10 && MONTH > 10){
-        return `0${DAY}-${MONTH}-${YEAR}`;
+    else if (DAY < 10 && MONTH >= 10){
+        return `${MONTH}-0${DAY}-${YEAR}`;
     }
-    else if (DAY > 10 && MONTH < 10){
-        return `${DAY}-0${MONTH}-${YEAR}`;
+    else if (DAY >= 10 && MONTH < 10){
+        return `0${MONTH}-${DAY}-${YEAR}`;
     }
-    return `${DAY}-${MONTH}-${YEAR}`;
+    return `${MONTH}-${DAY}-${YEAR}`;
 } 
 
 console.log(formataData(new Date(2022,3,4)))
--- JavaScript/loopsExercises/countEvenBetweenValues.js
+++ JavaScript/loopsExercises/countEvenBetweenValues.js
@@ -1,24 +1,15 @@
-
 function contaParesEntreDoisValores(valor1, valor2){
     // escreve aqui a função
-    let contator = 0;
-
     if (valor1 === valor2) {
         return "Por favor introduza dois valores diferentes."
     }
-    else if (valor1 < valor2){
-        for (let i = valor1; i <= valor2; i++){
-            if (i % 2 === 0){
-                contator++;
-            }
+    const maxValue = Math.max(valor1, valor2);
+    const minValue = Math.min(valor1, valor2);
+    let evenCounter = 0;
+    for (let i = minValue; i <= maxValue; i++){
+        if (i % 2 === 0){
+            evenCounter++;
         }
     }
-    else{
-        for (let i = valor2; i <= valor1; i++){
-            if (i % 2 === 0){
-                contator++;
-            }
-        }
-    }
-    return contator;
+    return evenCounter;
 }
